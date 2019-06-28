4aefabcad3253a6cb5e5dd30c27c271b0a383eca
Your Name <you@example.com>
2019-06-28 11:00:25 +0530

    Add project files.

---
+++ StringFunctions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String subStr = StringMethods.StringGetMethods.GetSubstring("Hello World", 2, 4);
+            Console.WriteLine(subStr);
+        }
+    }
+}
---
+++ StringMethods/StringGetMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringMethods
+{
+    public class StringGetMethods
+    {
+        public static String GetSubstring(String mainString, int startPosition,int numOfCharacters)
+        {
+            String subString = "";
+            for(int i = startPosition - 1; i <= numOfCharacters; i++)
+            {
+                subString = subString+mainString[i];
+            }
+            return subString;
+
+        }
+    }
+}
